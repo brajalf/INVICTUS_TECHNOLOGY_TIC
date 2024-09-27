0e24ecb04059563f4fce8a1022f2e08bbf41aed2
brajalf <111999245+brajalf@users.noreply.github.com>
2024-09-27 15:54:01 -0500

    Actuliación add Analytics

--- js/schema.js
+++ js/schema.js
@@ -2,11 +2,11 @@ const schemaData = {
     "@context": "https://schema.org",
     "@type": "Organization",
     "name": "Invictus Technology TIC",
-    "url": "https://www.tusitio.com",
-    "logo": "https://www.tusitio.com/images/logo.png",
+    "url": "https://www.invictustechnologytic.com",
+    "logo": "https://www.invictustechnolgytic.com/images/logov.png",
     "contactPoint": {
       "@type": "ContactPoint",
-      "telephone": "+57-123-4567",
+      "telephone": "+57-320-451-8979",
       "contactType": "Customer Service"
     }
   };
