3d83d32dce464b446dc8e8dd718783bbd62fdf4d
brajalf <111999245+brajalf@users.noreply.github.com>
2024-09-27 16:03:40 -0500

    Actualizacion

--- js/analytics.js
+++ js/analytics.js
@@ -1,11 +1,5 @@
-(function() {
-    var script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-N74NFH6WDC';
-    script.async = true;
-    document.head.appendChild(script);
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-N74NFH6WDC');
-})();
+  gtag('config', 'G-N74NFH6WDC');
